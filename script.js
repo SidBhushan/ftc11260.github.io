2f3c37ccd21220949c6f28b451be44475dbf3a1b
SidBhushan <sidhbhushan@gmail.com>
2017-02-26 20:49:51 -0700

    updated

--- script.js
+++ script.js
@@ -13,6 +13,22 @@ function submit()
 
     localStorage.setItem(teamNum + "-entryNum", teamEntryNum)
 
+    let teamList = localStorage.getItem("teams")
+
+    if(teamList == null)
+    {
+        localStorage.setItem("teams",teamNum)
+    }
+    else
+    {
+        let teamArray = teamList.split(",")
+
+        if(teamArray.indexOf(teamNum) == -1)
+       {
+           teamArray.push(teamNum)
+       }  
+      localStorage.setItem("teams", teamArray.toString())
+    }
     let autonShots = document.getElementById("autonShots").value
     localStorage.setItem(teamNum + "-" + teamEntryNum + "-autonShots", autonShots)
 
@@ -51,6 +67,28 @@ function search()
         let cap_ball = localStorage.getItem(team + "-" + i + "-cap")
         let note = localStorage.getItem(team + "-" + i + "-notes")
 
-        document.getElementById("table").innerHTML += `<tr><td>${i}</td><td>${auton_shots}</td><td>${auton_beacons}</td><td>${park}</td><td>${teleop_shots}</td><td>${teleop_beacons}</td><td>${cap_ball}</td><td class="notes">${note}</td></tr>`
+        document.getElementById("table").innerHTML += `<tr><td>${i}</td><td>${team}</td><td>${auton_shots}</td><td>${auton_beacons}</td><td>${park}</td><td>${teleop_shots}</td><td>${teleop_beacons}</td><td>${cap_ball}</td><td class="notes">${note}</td></tr>`
+    }
+}
+
+function showAll()
+{
+    let array = localStorage.getItem("teams").split(",")
+    for(let x in array)
+    {
+        let team = array[x]
+        let entryNum = localStorage.getItem(team + "-entryNum")
+        for(let i = 1; i <= entryNum; i ++)
+        {
+            let auton_shots = localStorage.getItem(team + "-" + i + "-autonShots")
+            let auton_beacons = localStorage.getItem(team + "-" + i + "-autonBeacons")
+            let park = localStorage.getItem(team + "-" + i + "-park")
+            let teleop_shots = localStorage.getItem(team + "-" + i + "-teleopShots")
+            let teleop_beacons = localStorage.getItem(team + "-" + i + "-teleopBeacons")
+            let cap_ball = localStorage.getItem(team + "-" + i + "-cap")
+            let note = localStorage.getItem(team + "-" + i + "-notes")
+
+            document.getElementById("table").innerHTML += `<tr><td>${i}</td><td>${team}</td><td>${auton_shots}</td><td>${auton_beacons}</td><td>${park}</td><td>${teleop_shots}</td><td>${teleop_beacons}</td><td>${cap_ball}</td><td class="notes">${note}</td></tr>`
+        }
     }
 }
